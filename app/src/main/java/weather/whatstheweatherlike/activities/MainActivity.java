36cd1e2e3d46f59c2e26ce6652dd8c1162dfc8ad
luca <luca.gugole99@gmail.com>
2018-09-04 23:28:59 +0200

    api weather manager created

--- app/src/main/java/weather/whatstheweatherlike/activities/MainActivity.java
+++ app/src/main/java/weather/whatstheweatherlike/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package weather.whatstheweatherlike;
+package weather.whatstheweatherlike.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import weather.whatstheweatherlike.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/weather/whatstheweatherlike/services/WeatherManager.java
@@ -0,0 +1,6 @@
+package weather.whatstheweatherlike.services;
+
+public class WeatherManager {
+
+    private final static String key = "ba331c2494b96ae8ddaefdc0f839c18d";
+}
